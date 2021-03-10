fe9d91880cb29fae10814e9ec2ce758c40c50204
MahmoudM828060 <828060@pdsb.net>
2021-03-10 14:22:11 -0500

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+ function getName(){
+    let name = prompt("What's your name?");
+    alert("Hello " + name);
+  }
+
+  function comingsoon() {
+    alert("This assignment has not been completed.");
+  }
