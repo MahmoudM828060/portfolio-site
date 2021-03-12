ed44102bc7ce8e91bac92a6f0ca02b2a457e49e1
MahmoudM828060 <828060@pdsb.net>
2021-03-11 23:12:27 -0500

    added comments on code

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,10 @@
+ /*code for name button on homepage*/
  function getName(){
     let name = prompt("What's your name?");
     alert("Hello " + name);
   }
 
+/*code for all assignment buttons*/
   function comingsoon() {
     alert("This assignment has not been completed.");
   }
