70ab9e24c316588beef32e4aa87eb22ebe7cc9ff
MahmoudM828060 <78868464+MahmoudM828060@users.noreply.github.com>
2021-04-20 21:12:42 -0400

    assignment

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,19 @@
     alert("Hello " + name);
   }
 
-/*code for all assignment buttons*/
-  function comingsoon() {
-    alert("This assignment has not been completed.");
+  /* Assignment button 1, find larger number button*/
+function largernumber(){
+  let num1 = parseInt(prompt("Enter your first number: "));
+  let num2 =  parseInt(prompt("Enter your second number: "));
+  let output = ""
+  if (num1 > num2){ //if first number is larger than second, output first
+    output = num1;
+  } else if (num1 == num2){ // otherwise, if they are equal, output this
+    output = "The numbers you entered are equal!";
+  } else if (num2 > num1){ // if num 2 is larger, output this
+    output = num2;
+  } else { // if there arent sufficient numbers for the function to work
+    output = "Invalid Output";
   }
+  document.getElementById("largerNum").innerHTML = output;
+}
